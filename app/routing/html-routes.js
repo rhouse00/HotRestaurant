ddf86f20edfba5140c04f7a2318528a4f5dfc7f2
Serene Talia Namini <talia.namini@gmail.com>
2017-01-07 11:36:40 -0800

    revised routes

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -3,20 +3,17 @@ var path = require("path");
 
 module.exports = function(app){
 
-    app.get('/tables', function(req, res){
-          // res.sendFile(path.join(__dirname, "/public/survey.html"));
-          res.send("../public/tables.html");
+    app.get('/reservations', function(req, res){
+          res.sendFile(path.join(__dirname, "../public/reservation.html"));
     });
 
-    app.get('/reservations', function(req, res){
-          // res.sendFile(path.join(__dirname, "/public/survey.html"));
-          res.send("../public/reservation.html");
+    app.get('/tables', function(req, res){
+          res.sendFile(path.join(__dirname, "../public/tables.html"));
     });
 
     app.use(function(req, res){
       res.send("/public/home.html");
     });
     //other routes..
-
 
 };
